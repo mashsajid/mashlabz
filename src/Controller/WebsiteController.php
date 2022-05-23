8e11dfa8491f0bbaab3a29fc1a9d24895d90e6a7
mashsajid <mashsajid@protonmail.com>
2022-05-23 23:28:58 +0600

    push

---
+++ src/Controller/WebsiteController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WebsiteController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index() : Response
+    {
+        return $this->render('website/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/contact", name="app_contact")
+     */
+    public function contact() : Response
+    {
+        return $this->render('website/contact.html.twig', []);
+    }
+
+    /**
+     * @Route("/cart", name="app_cart")
+     */
+    public function cart() : Response
+    {
+        return $this->render('website/cart.html.twig', []);
+    }
+}
