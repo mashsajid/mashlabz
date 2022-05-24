0a9254ce535e817d29cf1a592d170953662440f7
mashsajid <mashsajid@protonmail.com>
2022-05-24 14:42:54 +0600

    fixed cart dropdown

--- src/Controller/WebsiteController.php
+++ src/Controller/WebsiteController.php
@@ -30,7 +30,7 @@ class WebsiteController extends AbstractController
     public function cart() : Response
     {
         return $this->render('website/cart.html.twig', [
-            'l' => [1, 2, 3, 4, 5, 6, 7]
+            'l' => [1, 2]
         ]);
     }
 }
