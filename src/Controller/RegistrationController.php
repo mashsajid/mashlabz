0ab89a822566979b1f46a74fb7ee9891d6c7ae12
mashsajid <mashsajid@protonmail.com>
2022-06-14 14:12:54 +0600

    commit

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -29,6 +29,9 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppCustomAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_index');
+        }
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user,[
             'attr' => ['class' => 'row contact_form']
