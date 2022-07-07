c8a73ac6150ab498c1cd8fe3ff0fbf5a3d43cef3
mashsajid <mashsajid@protonmail.com>
2022-07-08 05:47:16 +0600

    cart and checkout added!

--- src/Controller/WebsiteController.php
+++ src/Controller/WebsiteController.php
@@ -31,4 +31,49 @@ class WebsiteController extends AbstractController
     {
         return $this->render("_part/_searchbar.html.twig");
     }
+
+    /**
+     * @Route("/cart", name="app_cart")
+     */
+    public function cart() : Response
+    {
+        $products = [
+            1 => [
+                'name' => 'Blue denim shirt',
+                'image' => 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/12a.webp',
+                'color' => 'blue',
+                'size' => 'M',
+                'quantity' => 1,
+                'price' => 17.99
+            ],
+            2 => [
+                'name' => 'Red denim shirt',
+                'image' => 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/12a.webp',
+                'color' => 'red',
+                'size' => 'M',
+                'quantity' => 4,
+                'price' => 17.99
+            ]
+        ];
+        $cartPrice = 0.00;
+        $totalQuantity = 0;
+        foreach ($products as $product)
+        {
+            $cartPrice += $product['price'] * $product['quantity'];
+            $totalQuantity += $product['quantity'];
+        }
+        return $this->render("user/cart.html.twig", [
+            'products' => $products,
+            'cartPrice' => $cartPrice,
+            'totalquantity' => $totalQuantity
+        ]);
+    }
+
+    /**
+     * @Route("checkout", name="app_checkout")
+     */
+    public function checkout() : Response
+    {
+        return $this->render('user/checkout.html.twig', []);
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -76,5 +76,4 @@ Encore
     // uncomment if you're having problems with a jQuery plugin
     // .autoProvidejQuery()
 ;
-
 module.exports = Encore.getWebpackConfig();
